4e7af14f2ab4d19c90688227bdc2f4e350aedf71
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-01-30 19:05:26 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,14 @@
+// Now I am lorning the function of js
+
+function startFan(){
+    console.log('weak up ')
+    console.log('then wack and go there')
+    console.log('then click the swich')
+
+}
+
+startFan();
+console.log ('then go to the bed and sleep')
+console.log ('then get up early in the morning.')
+
+
---
+++ return.js
@@ -0,0 +1,14 @@
+// This is the return side 
+
+
+function add (number1 , number2){
+    console.log (number1 , number2);
+    var sum = number1 + number2;
+    // console.log (sum);
+    return sum;
+}
+
+// add (45, 15);
+
+var total = add(80,20)
+console.log('Total:',total)
