055f84250f4a96bf27bd2d9ddc8e4a8c5a122193
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-01-30 20:58:14 +0600

    done

---
+++ properties.js
@@ -0,0 +1,14 @@
+// Now I am going to bye some vagetable for cook .
+
+// This is the list of te vagetable .
+
+
+var shoping = { 
+    tometo:"1kg",
+    alu:"1kg",
+    holud:'1kg',
+    biriani:"1peket",
+    chips:1
+}
+
+console.log(shoping)
